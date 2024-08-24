9c8fe165a8dd1643bcd1e4301b02ffc43201af64
AdishtJaglan <adisht7524@gmail.com>
2024-08-24 16:55:11 +0530

    create put, patch endpoints for dynamic model

--- server/controllers/dynamicControllers.js
+++ server/controllers/dynamicControllers.js
@@ -69,3 +69,151 @@ export const getDynamicModelData = async (req, res) => {
     });
   }
 };
+
+/**
+ * @GET Read - Get a single data entry for a specific template.
+ * @AUTH -
+ * @ENDPOINT /api/dynamic/:templateId/:id -> id is dynamic model entry id
+ * @RES_BODY => { message, data } data is the single entry retrieved from the model
+ */
+export const getSingleDataFromDynamicModel = async (req, res) => {
+  try {
+    const { templateId, id } = req.params;
+    const template = await Template.findById(templateId);
+
+    if (!template) {
+      return res.status(404).json({ message: "Template not found." });
+    }
+
+    const DynamicModel = createFields(templateId, template.fields);
+    const data = await DynamicModel.findById(id);
+
+    if (!data) {
+      return res
+        .status(404)
+        .json({ message: "No data added for these fields." });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Fetch dynamic model data.", data: data });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error fetching data from dynamic model.",
+      error: error.message,
+    });
+  }
+};
+
+/**
+ * @PATCH Update isMailSent status for a single data entry
+ * @AUTH -
+ * @ENDPOINT /api/dynamic/:templateId/:id -> is dynamic model entry id
+ * @RES_BODY => { message, data } data is the updated entry
+ */
+export const setMailSent = async (req, res) => {
+  try {
+    const { templateId, id } = req.params;
+    const template = await Template.findById(templateId);
+
+    if (!template) {
+      return res.status(404).json({ message: "Template not found." });
+    }
+
+    const DynamicModel = createFields(templateId, template.fields);
+    const patchedData = await DynamicModel.findByIdAndUpdate(
+      id,
+      { isMailSent: true },
+      { new: true, runValidators: true }
+    );
+
+    if (!patchedData) {
+      return res
+        .status(404)
+        .json({ message: "Data entry not found while patching." });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Set isMailSent as true.", data: patchedData });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Unable to set isMailSent as true.",
+      error: error.message,
+    });
+  }
+};
+
+/**
+ * @PUT Update - Update a single data entry for a specific template.
+ * @AUTH -
+ * @ENDPOINT /api/dynamic/:templateId/:id -> is dynamic model entry id
+ * @REQ_BODY => { req.body } fields to update
+ * @RES_BODY => { message, data } data is the updated entry
+ */
+export const updateDynamicModel = async (req, res) => {
+  try {
+    const { templateId, id } = req.params;
+    const updateBody = req.body;
+    const template = await Template.findById(templateId);
+
+    if (!template) {
+      return res.status(404).json({ message: "Template not found." });
+    }
+
+    const DynamicModel = createFields(templateId, template.fields);
+    const updatedData = await DynamicModel.findByIdAndUpdate(id, updateBody, {
+      new: true,
+    });
+
+    if (!updatedData) {
+      return res
+        .status(404)
+        .json({ message: "Entry not found in dynamic model." });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Successfully updated entry.", data: updatedData });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Unable to update dynamic model entry.",
+      error: error.message,
+    });
+  }
+};
+
+/**
+ * @DELETE Delete - Delete a single data entry for a specific template.
+ * @AUTH -
+ * @ENDPOINT /api/dynamic/:templateId/:id -> id is dynamic model entry id
+ * @RES_BODY => { message } confirmation of deletion
+ */
+export const deleteDynamicModelData = async (req, res) => {
+  try {
+    const { templateId, id } = req.params;
+    const template = await Template.findById(templateId);
+
+    if (!template) {
+      return res.status(404).json({ message: "Template not found." });
+    }
+
+    const DynamicModel = createFields(templateId, template.fields);
+    const deletedData = await DynamicModel.findByIdAndDelete(id);
+
+    if (!deletedData) {
+      return res
+        .status(404)
+        .json({ message: "Dynamic model entry not found." });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Data successfully deleted.", data: deletedData });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error deleting dynamic model data.",
+      error: error.message,
+    });
+  }
+};
--- server/routes/dynamicRoutes.js
+++ server/routes/dynamicRoutes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import {
   addDataToDynamicModel,
+  deleteDynamicModelData,
   getDynamicModelData,
+  getSingleDataFromDynamicModel,
+  setMailSent,
+  updateDynamicModel,
 } from "../controllers/dynamicControllers.js";
 
 const router = Router();
@@ -23,5 +27,38 @@ router.post("/:templateId", addDataToDynamicModel);
  *              data is the data retrieved from that model
  */
 router.get("/:templateId", getDynamicModelData);
+
+/**
+ * @GET Read - Get a single data entry for a specific template.
+ * @AUTH -
+ * @ENDPOINT /api/dynamic/:templateId/:id -> id is dynamic model entry id
+ * @RES_BODY => { message, data } data is the single entry retrieved from the model
+ */
+router.get("/:templateId/:id", getSingleDataFromDynamicModel);
+
+/**
+ * @PUT Update - Update a single data entry for a specific template.
+ * @AUTH -
+ * @ENDPOINT /api/dynamic/:templateId/:id -> id is dynamic model entry id
+ * @REQ_BODY => { req.body } fields to update
+ * @RES_BODY => { message, data } data is the updated entry
+ */
+router.put("/:templateId/:id", updateDynamicModel);
+
+/**
+ * @PATCH Update isMailSent status for a single data entry
+ * @AUTH -
+ * @ENDPOINT /api/dynamic/:templateId/:id -> is dynamic model entry id
+ * @RES_BODY => { message, data } data is the updated entry
+ */
+router.patch("/:templateId/:id", setMailSent);
+
+/**
+ * @DELETE Delete - Delete a single data entry for a specific template.
+ * @AUTH -
+ * @ENDPOINT /api/dynamic/:templateId/:id -> id is dynamic model entry id
+ * @RES_BODY => { message } confirmation of deletion
+ */
+router.delete("/:templateId/:id", deleteDynamicModelData);
 
 export default router;
